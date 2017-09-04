8e826e1bf14f1c92e2500e28d2c8fde26a84b79e
winuz49 <zjwinuj@163.com>
2017-09-04 08:53:32 +0800

    add cifar

---
+++ cifar/cifar10_cnn.py
@@ -0,0 +1,11 @@
+import tensorflow
+import cifar10_input
+import cifar10
+
+max_size = 3000
+batch_size = 128
+data_dir = "CIFAR_data/cifar-10-batches-py"
+
+print 'hhh'
+
+cifar10.maybe_download_and_extract()
