6a62d7d64c2a7b59ea7b3686cf562e183de8861c
winuz49 <zjwinuj@163.com>
2017-12-18 19:26:11 +0800

    modify train

--- speech_recognition/train.py
+++ speech_recognition/train.py
@@ -1,5 +1,16 @@
 import tensorflow as tf
+from glob import glob
+import re
+import os
+data_dir = './data'
+POSSIBLE_LABELS = 'yes no up down left right on off stop go silence unknown'.split()
+id2name = {i: name for i, name in enumerate(POSSIBLE_LABELS)}
+name2id = {name: i for i, name in enumerate(POSSIBLE_LABELS)}
 
+def load_data():
+    pattern = re.compile("(.+\/)?(\w+)\/([^_]+)_.+wav")
+    all_files = glob(os.path.join(data_dir, 'train/audio/*/*wav'))
+    print all_files
 
 if __name__ == '__main__':
     print tf.__version__
